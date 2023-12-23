1c476480378c4380462762c92d969e7b2333c4a7
Sakurai Yuki <sakurai0803@gmail.com>
2023-12-23 21:04:49 +0900

    ✨ Model追加

---
+++ ProductActivationService/Models/BaseEntity.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace ProductActivation.Service.Models
+{
+  public class BaseEntity
+  {
+    [Column("created_at")]
+    [Comment("登録日時")]
+    public DateTime? Created { get; set; }
+
+    [Column("updated_at")]
+    [Comment("更新日時")]
+    public DateTime? Updated { get; set; }
+
+    [Column("deleted_at")]
+    [Comment("削除日時")]
+    public DateTime? Deleted { get; set; }
+
+    [Column("is_active")]
+    [Comment("状態フラグ:true:利用可可能、null:削除済み")]
+    public bool? IsAlive { get; set; } = true;
+  }
+}
---
+++ ProductActivationService/Models/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace ProductActivation.Service.Models
+{
+  [Table("customer")]
+  [Comment("カスタマーテーブル")]
+  public class Customer : BaseEntity
+  {
+    [Key]
+    [Required]
+    [Column("id", TypeName = "bigint")]
+    [Comment("サンプルID")]
+    public long SampleId { get; set; }
+
+    [Required]
+    [Column("sample_name", TypeName = "varchar")]
+    [StringLength(20)]
+    [Comment("サンプル名称")]
+    public long SampleName { get; set; }
+  }
+}
