bfbeb7f84c0097cc07266643091d98aa25255e07
Sakurai Yuki <sakurai0803@gmail.com>
2023-12-24 07:32:23 +0900

    ✨ リクエストを追加

---
+++ ProductActivationService/Requests/ListCustomerRequest.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductActivationService.Requests
+{
+  public class ListCustomerRequest
+  {
+    [FromQuery(Name = "name")]
+    public string? Name { get; set; }
+  }
+}
